6aaf881ad520f3b88f310ea3404a72fa7f2637bf
cjw <1468733552@qq.com>
2019-09-04 14:59:17 +0800

    优化 template --reset 命令实现

--- src/config/template.ts
+++ src/config/template.ts
@@ -1,57 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+
 export type Template = {
     templateName: string,
     remoteAddress: string
 }
-// const templates: Template[] = [
-//     {
-//         templateName: 'vue-project',
-//         remoteAddress: 'https://github.com/JohnApache/events-proxy#test-cli'
-//     },
-//     {
-//         templateName: 'react-project',
-//         remoteAddress: 'https://github.com/JohnApache/events-proxy#test-cli'
-//     },
-//     {
-//         templateName: 'vue-component',
-//         remoteAddress: 'https://github.com/JohnApache/events-proxy#test-cli'
-//     },
-//     {
-//         templateName: 'react-component',
-//         remoteAddress: 'https://github.com/JohnApache/events-proxy#test-cli'
-//     },
-//     {
-//         templateName: 'jquery-project',
-//         remoteAddress: 'https://github.com/JohnApache/events-proxy#test-cli'
-//     },
-//     {
-//         templateName: 'jquery-plugin',
-//         remoteAddress: 'https://github.com/JohnApache/events-proxy#test-cli'
-//     },
-//     {
-//         templateName: 'js-lib',
-//         remoteAddress: 'https://github.com/JohnApache/events-proxy#test-cli'
-//     },
-//     {
-//         templateName: 'koa-project',
-//         remoteAddress: 'https://github.com/JohnApache/events-proxy#test-cli'
-//     },
-//     {
-//         templateName: 'express-project',
-//         remoteAddress: 'https://github.com/JohnApache/events-proxy#test-cli'
-//     },
-//     {
-//         templateName: 'egg-project',
-//         remoteAddress: 'https://github.com/JohnApache/events-proxy#test-cli'
-//     },
-//     {
-//         templateName: 'node-lib',
-//         remoteAddress: 'https://github.com/JohnApache/events-proxy#test-cli'
-//     },
-// ]
 
-// export default templates;
-import fs from 'fs';
-import path from 'path';
 export const GetTemplates = (): Template[] => {
     const templateFilePath = path.resolve(__dirname, '../../assets/template.json');
     const buffer = fs.readFileSync(templateFilePath);
@@ -66,9 +20,8 @@ export const UpdateTemplates = (templates: Template[]): void => {
 }
 
 export const ResetTemplates = (): void => {
+    const templateFilePath = path.resolve(__dirname, '../../assets/template.json');
     const templateBakFilePath = path.resolve(__dirname, '../../assets/template.json.bak');
     const buffer = fs.readFileSync(templateBakFilePath);
-    const json = buffer.toString();
-    if(!json) return UpdateTemplates([]);
-    return UpdateTemplates(JSON.parse(json));
+    fs.writeFileSync(templateFilePath, buffer);
 }
